4c58a0925ee00aa7bb1556deb6340b7e35350a78
rohitgthm <rohit01ofc@gmail.com>
2025-11-23 22:32:12 +0530

    update dev script to enable host and configure Vite server settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+export default defineConfig(
+  {
+    plugins: [react(), tailwindcss()],
+  },
+  {
+    server: {
+      host: true,
+    },
+  }
+);
